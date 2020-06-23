c4e3a5a7ebf79b3a14a0a2618d9ff95797665794
Hunter Piggot <hunterpiggot@gmail.com>
2020-06-22 19:43:34 -0600

    made it work

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ function randomRBG() {
 	const r = Math.floor(Math.random() * 256);
 	const g = Math.floor(Math.random() * 256);
 	const b = Math.floor(Math.random() * 256);
-	return `rbg(${r}, ${g}, ${b})`;
+	return `rgb(${r},${g},${b})`;
 }
-const h1 = document.querySelector('h1');
 
-const letters = document.querySelectorAll('.letters');
+const letters = document.querySelectorAll('.letter');
 const intervalId = setInterval(function() {
 	for (let letter of letters) {
 		letter.style.color = randomRBG();
-		h1.style.color = randomRBG;
-		// console.log(randomRBG());
 	}
 }, 1000);
