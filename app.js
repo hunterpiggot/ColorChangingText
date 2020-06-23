0c7bf6afad9efea891b32983b86b3dce5a61ada2
Hunter Piggot <hunterpiggot@gmail.com>
2020-06-22 19:31:28 -0600

    fixing bugs

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ function randomRBG() {
 	const b = Math.floor(Math.random() * 256);
 	return `rbg(${r}, ${g}, ${b})`;
 }
+const h1 = document.querySelector('h1');
 
-const letters = document.querySelector('.letters');
+const letters = document.querySelectorAll('.letters');
 const intervalId = setInterval(function() {
 	for (let letter of letters) {
 		letter.style.color = randomRBG();
+		h1.style.color = randomRBG;
+		// console.log(randomRBG());
 	}
 }, 1000);
