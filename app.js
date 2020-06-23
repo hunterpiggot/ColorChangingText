a357623dbdae9b6ce1b9596cff67ee497e361f4e
Hunter Piggot <hunterpiggot@gmail.com>
2020-06-22 18:57:26 -0600

    added h2 and changed color time

---
+++ app.js
@@ -0,0 +1,13 @@
+function randomRBG() {
+	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
+	const b = Math.floor(Math.random() * 256);
+	return `rbg(${r}, ${g}, ${b})`;
+}
+
+const letters = document.querySelector('.letters');
+const intervalId = setInterval(function() {
+	for (let letter of letters) {
+		letter.style.color = randomRBG();
+	}
+}, 1000);
